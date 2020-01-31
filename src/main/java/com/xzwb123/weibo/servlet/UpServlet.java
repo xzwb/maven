e5739103f9c54f99a6f06c50b4e1a2504607ee4c
xzwb <1478863479@qq.com>
2020-01-31 13:38:58 +0800

    上传文件时不添加

--- src/main/java/com/xzwb123/weibo/servlet/UpServlet.java
+++ src/main/java/com/xzwb123/weibo/servlet/UpServlet.java
@@ -23,25 +23,37 @@ public class UpServlet extends HttpServlet {
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("utf-8");
         resp.setContentType("text/html;charset=utf-8");
+        /*String h = req.getParameter("img");
+        System.out.println(h);*/
         Part part = req.getPart("img");
+        /*if (part == null) {
+            System.out.println("xxx");
+        } else {
+            System.out.println("yyy");
+        }*/
         /*String filename = part.getHeader("Content-Disposition");
         System.out.println(filename);*/
         String filename = part.getSubmittedFileName();
+        if ("".equals(filename)) {
+            System.out.println("xzzz");
+        } else {
+            System.out.println("ccc");
+        }
         // System.out.println(f);
         /*String f1 = part.getName();
         System.out.println(f1);*/
-        long timeInMillis = Calendar.getInstance().getTimeInMillis();
-        filename = timeInMillis + ""/*+ filename*/;
-        // System.out.println(filename);
-        String url = /*req.getSession().getServletContext().getRealPath("")*/  filename;
-        try {
-            part.write(String.format("%s", url));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        NewFile newFile = new NewFile();
-        newFile.setFilename(url);
-        NewFileService newFileService = new NewFileService();
-        newFileService.newFileService(newFile);
+//        long timeInMillis = Calendar.getInstance().getTimeInMillis();
+//        filename = timeInMillis + ""/*+ filename*/;
+//        // System.out.println(filename);
+//        String url = /*req.getSession().getServletContext().getRealPath("")*/  filename;
+//        try {
+//            part.write(String.format("%s", url));
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        NewFile newFile = new NewFile();
+//        newFile.setFilename(url);
+//        NewFileService newFileService = new NewFileService();
+//        newFileService.newFileService(newFile);
     }
 }
